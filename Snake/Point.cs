98f20284fb2ea8b8ae1db8eca4e1180081f0f535
Gurdand <zmei83666@gmail.com>
2019-01-24 21:02:04 +0300

    add classes

--- Snake/Point.cs
+++ Snake/Point.cs
@@ -20,10 +20,42 @@ namespace Snake
 
         }
 
+        public Point(Point p)
+        {
+            x = p.x;
+            y = p.y;
+            sym = p.sym;
+        }
+
+        public void Move(int offset, Direction direction)
+        {
+            if(direction == Direction.RIGHT)
+            {
+                x += offset;
+            }
+            else if (direction == Direction.LEFT)
+            {
+                x -= offset;
+            }
+            else if (direction == Direction.UP)
+            {
+                y += offset;
+            }
+            else if(direction== Direction.DOWN)
+            {
+                y -= offset;
+            }
+        }
+
         public void Drow()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
+        }
+
+        public override string ToString()
+        {
+            return x + "," + y + "," + sym;
         }
     }
 }
--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,11 +10,20 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Drow();
+            Console.SetBufferSize(80, 25);
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Drow();
+            HorisontalLine upLine = new HorisontalLine(0, 78, 0, '+');
+            HorisontalLine downLine = new HorisontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 0, 24, '+');
+            VerticalLine rightLine = new VerticalLine(78, 0, 24, '+');
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
+
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
 
             Console.ReadKey();
         }
